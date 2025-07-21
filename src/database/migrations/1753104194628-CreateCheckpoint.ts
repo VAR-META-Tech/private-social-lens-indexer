ef6f6d3c58232a951b453e27f942abd786fd8762
VaughnMYA <vaughn@mysoft.co.za>
2025-07-21 17:27:20 +0200

    fix(migration): enum error

--- src/database/migrations/1753104194628-CreateCheckpoint.ts
+++ src/database/migrations/1753104194628-CreateCheckpoint.ts
@@ -1,11 +1,12 @@
 import { MigrationInterface, QueryRunner } from 'typeorm';
 
-export class CreateCheckPointCheckPoint1753073027159
-  implements MigrationInterface
-{
-  name = 'CreateCheckPointCheckPoint1753073027159';
+export class CreateCheckpoint1753104194628 implements MigrationInterface {
+  name = 'CreateCheckpoint1753104194628';
 
   public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `CREATE TYPE "public"."checkpoint_querytype_enum" AS ENUM('fetch-staking', 'fetch-unstaking', 'fetch-request-reward', 'refresh-failed-staking', 'refresh-failed-unstaking', 'refresh-failed-request-reward')`,
+    );
     await queryRunner.query(
       `CREATE TABLE "checkpoint" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "toBlockNumber" numeric NOT NULL, "fromBlockNumber" numeric NOT NULL, "blockTimestamp" numeric NOT NULL, "queryType" "public"."checkpoint_querytype_enum" NOT NULL, "isFailed" boolean NOT NULL, "createdAt" TIMESTAMP NOT NULL DEFAULT now(), "updatedAt" TIMESTAMP NOT NULL DEFAULT now(), CONSTRAINT "PK_fea86db187949398f8b614f730a" PRIMARY KEY ("id"))`,
     );
@@ -13,5 +14,6 @@ export class CreateCheckPointCheckPoint1753073027159
 
   public async down(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(`DROP TABLE "checkpoint"`);
+    await queryRunner.query(`DROP TYPE "public"."checkpoint_querytype_enum"`);
   }
 }
