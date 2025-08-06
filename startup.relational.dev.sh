452610c9de0717e327f66ab397a07320ae27d030
Nam Dang <namquoc.dev@gmail.com>
2025-08-06 16:46:20 +0700

    ci(migraion): manully

--- startup.relational.dev.sh
+++ startup.relational.dev.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 set -e
 
-npm run migration:run
-npm run seed:run:relational
+# manual migration and seeding
+# npm run migration:run
+# npm run seed:run:relational
 npm run start:prod
